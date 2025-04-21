07de2df6fd6432473e7d1ddcb167881168a98585
Obua Stella <stellaobua123@gmail.com>
2025-04-21 01:19:10 +0100

    login ui

---
+++ src/pages/Auth/Login/Functions/LoginForm.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { Eye, EyeOff, Loader } from "lucide-react";
+import { toast } from "react-toastify";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../../components/firebase";
+import { useNavigate } from "react-router-dom";
+export default function LoginForm() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const payload = {
+      email: email,
+      password: password,
+    };
+    try {
+      console.log("Payload:", payload);
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User logged in succesfully!");
+      toast.success("User logged in successfully");
+      navigate("/dashboard");
+      setIsLoading(false);
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message, {
+        position: "top-right",
+      });
+      setIsLoading(false);
+    }
+  };
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-8 w-full md:w-md mb-6 mx-auto"
+      >
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-secondary focus:outline-none focus:ring-0 focus:border-secondary peer"
+            placeholder=" "
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-secondary peer-focus:dark:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            name="floating_password"
+            id="floating_password"
+            className="block py-2.5 px-0 pr-10 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-secondary focus:outline-none focus:ring-0 focus:border-secondary peer"
+            placeholder=" "
+            required
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-secondary peer-focus:dark:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
+          <div
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer pr-2"
+            onClick={() => setPasswordVisible(!passwordVisible)}
+          >
+            {passwordVisible ? <Eye size={20} /> : <EyeOff size={20} />}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="mt-8 m-auto cursor-pointer w-full text-white bg-secondary/80 hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  py-2.5 text-center"
+        >
+          {isLoading ? (
+            <Loader size={24} className="mx-auto animate-spin" />
+          ) : (
+            "Submit"
+          )}
+        </button>
+      </form>
+    </>
+  );
+}
--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import LoginForm from "./Functions/LoginForm";
 
 export default function Login() {
   return (
-    <div className="text-center">
-      <h1>Login</h1>
-      <Link to="/dashboard" className="border px-10">
-        Click here to go to dasboard
-      </Link>
+    <div className=" gap-y-3 flex justify-center items-center flex-col w-full h-screen">
+      <div className="w-[90%] md:w-auto rounded-md gap-y-3 shadow-md p-4 flex justify-center items-center flex-col">
+        <div className="ms-4 flex justify-center items-center w-32 h-32 rounded-full">
+          <img src="/images/logo.png" alt="logo" />
+        </div>
+        <h1 className="font-bold text-2xl text-center">Login</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 }
